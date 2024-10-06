c5936443b21fb0f782b7d173289124658ea97fdc
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-10-06 23:57:29 +0300

    Worked on sqlalchemy lab 2.

--- server/models.py
+++ server/models.py
@@ -11,22 +11,55 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # Relationship mapping customer to related review
+    reviews = db.relationship('Review', back_populates='customer')
+
+    # Association proxy to access items through reviews
+    items = association_proxy('reviews', 'item', creator=lambda item_obj: Review(item=item_obj))
+
+    serialize_rules = ('-reviews.customer',)
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # Relationship mapping item to related review
+    reviews = db.relationship('Review', back_populates='item')
+
+    serialize_rules = ('-reviews.item',)
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String, nullable=False)
+
+    # Foreign key to store customer id
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+
+    # Foreign key to store item id
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    # Relationship mapping review to related customer
+    customer = db.relationship('Customer', back_populates='reviews')
+
+    # Relationship mapping review to related item
+    item = db.relationship('Item', back_populates='reviews')
+
+    serialize_rules = ('-customer.reviews', '-item.reviews',)
